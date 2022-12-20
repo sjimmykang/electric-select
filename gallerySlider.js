737e1f4c03db658864a29ad0d70b026b36cfea75
Jimmy Kang <sjimmy.kang@gmail.com>
2022-12-20 05:47:01 -0500

    finished with gallery slide feature

--- gallerySlider.js
+++ gallerySlider.js
@@ -4,21 +4,54 @@ const gallerySlider = {};
 
 gallerySlider.leftArrowEl = document.querySelector('.leftArrow');
 gallerySlider.rightArrowEl = document.querySelector('.rightArrow');
-gallerySlider.gallImage1 = document.querySelector('.gallImage1');
-gallerySlider.gallImage2 = document.querySelector('.gallImage2');
-gallerySlider.gallImage3 = document.querySelector('.gallImage3');
+gallerySlider.gallImage1 = document.getElementById('gallImage1');
+gallerySlider.gallImage2 = document.getElementById('gallImage2');
+gallerySlider.gallImage3 = document.getElementById('gallImage3');
 
-gallerySlider.currentImage = function(classNum = '.gallImage1') {
-    if (classNum == '.gallImage1') {
+gallerySlider.currentCenter = function(idNum = 'gallImage1') {
+    if (idNum == 'gallImage1') {
         gallerySlider.leftArrowEl.style.opacity = 0.3;
-    } else if (classNum == '.gallImage3') {
+        gallerySlider.rightArrowEl.style.opacity = 1;
+
+        gallerySlider.currentImage = gallerySlider.gallImage1;
+        gallerySlider.nextImage = gallerySlider.gallImage2;
+    } else if (idNum == 'gallImage2') {
+        gallerySlider.leftArrowEl.style.opacity = 1;
+        gallerySlider.rightArrowEl.style.opacity = 1;
+        gallerySlider.previousImage = gallerySlider.gallImage1;
+        gallerySlider.currentImage = gallerySlider.gallImage2;
+        gallerySlider.nextImage = gallerySlider.gallImage3;
+    } else {
+        gallerySlider.leftArrowEl.style.opacity = 1;
         gallerySlider.rightArrowEl.style.opacity = 0.3;
+
+        gallerySlider.previousImage = gallerySlider.gallImage2;
+        gallerySlider.currentImage = gallerySlider.gallImage3;
     }
 };
 
 gallerySlider.changeImage = (event) => {
-    console.log(event.target);
-}
+    // console.log(event.target)
+    if (event.target.alt == 'next image' && gallerySlider.currentImage.id != 'gallImage3' ) {
+        gallerySlider.currentImage.classList.toggle('galleryMain');
+        gallerySlider.currentImage.classList.toggle('gallerySub');
+
+        gallerySlider.currentImage = gallerySlider.nextImage;
+        gallerySlider.currentCenter(gallerySlider.currentImage.id);
+
+        gallerySlider.currentImage.classList.toggle('galleryMain');
+        gallerySlider.currentImage.classList.toggle('gallerySub');
+    } else if (event.target.alt == 'previous image' && gallerySlider.currentImage.id != 'gallImage1' ) {
+        gallerySlider.currentImage.classList.toggle('galleryMain');
+        gallerySlider.currentImage.classList.toggle('gallerySub');
+
+        gallerySlider.currentImage = gallerySlider.previousImage;
+        gallerySlider.currentCenter(gallerySlider.currentImage.id);
+
+        gallerySlider.currentImage.classList.toggle('galleryMain');
+        gallerySlider.currentImage.classList.toggle('gallerySub');
+    }
+};
 
 gallerySlider.setupEventListener = () => {
 
@@ -28,8 +61,7 @@ gallerySlider.setupEventListener = () => {
 };
 
 gallerySlider.init = () => {
-    gallerySlider.currentImage();
-
+    gallerySlider.currentCenter();
     gallerySlider.setupEventListener();
 };
 
